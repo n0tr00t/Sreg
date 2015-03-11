fdbacd29c1f97f857b72e4c30c3c8022e3017f07
evi1m0 <evi1m0.bat@gmail.com>
2015-03-11 18:09:37 +0800

    second commit

---
+++ README.md
@@ -0,0 +1,135 @@
+### Sreg V1.0
+
+- AppName: Sreg [Search Registration]
+- Create:  2015-03-03
+- Author:  Dave, Evi1m0, Hel
+- Website: [http://buzz.beebeeto.com](http://buzz.beebeeto.com)
+
+
+Sreg可对使用者通过输入```email```、```phone```、```username```的返回用户注册的所有互联网护照信息，例如：
+
+    ➜  Sreg git:(master) python reg_test.py -h
+    usage: reg_test.py [-h] [-u USER] [-e EMAIL] [-c CELLPHONE]
+
+    Check how many Platforms the User registered.
+
+    optional arguments:
+      -h, --help    show this help message and exit
+      -u USER
+      -e EMAIL
+      -c CELLPHONE
+
+    ➜  Sreg git:(master) ✗ python reg_test.py -e test@test.com
+
+         .d8888b.
+        d88P  Y88b
+        Y88b.
+         "Y888b.  888d888 .d88b.  .d88b.
+            "Y88b.888P"  d8P  Y8bd88P"88b
+              "888888    88888888888  888
+        Y88b  d88P888    Y8b.    Y88b 888
+         "Y8888P" 888     "Y8888  "Y88888
+                                      888
+                                 Y8b d88P
+                                  "Y88P"
+
+    [*] App: Search Registration
+    [*] Version: V1.0(20150303)
+    [*] Website: buzz.beebeeto.com
+
+    [+] Email Checking: test@test.com
+
+    [购物] 淘男网 (http://www.51taonan.com/)
+    [IT] 51cto (http://www.51cto.com)
+    [娱乐] 一听音乐网 (http://www.1ting.com/)
+    [工具] 金山词霸 (http://www.iciba.com/)
+    [生活] 58同城 (http://www.58.com/)
+    [名站] 优酷 (http://www.youku.com)
+    [IT] 果壳网 (http://www.guokr.com/)
+    [购物] 好乐买 (http://www.okbuy.com/)
+    [旅行] 艺龙 (http://www.elong.com/)
+    [旅游] 凯撒旅游网 (http://www.caissa.com.cn/)
+    [旅游] 酷讯旅游网 (http://www.kuxun.cn)
+    [娱乐] 乐视网 (http://www.youku.com)
+    [IT] CSDN (http://www.csdn.net/)
+    [生活] 百合网 (http://www.baihe.com/)
+    [购物] 当当网 (http://www.dangdang.com/)
+
+    [+] Results the save path: ./reports/email_test@test.com.html
+    
+Sreg一共有三种查询方式：
+ 
+  - 用户名
+  - 手机
+  - 邮箱
+
+查询完成后，Sreg会返回给使用者一个精致的html页面供以查看。
+    
+### Plugin (SDK)
+
+编写网站注册查询插件非常简单，首先将想要进行编写的网站在```/plugins/```建立对应```website.json```文件。
+
+    {
+        "information":{
+            "author" : "evi1m0",
+            "date" : "2015/03/10",
+            "name" : "PPTV",
+            "website" : "http://www.pptv.com/",
+            "category" : "娱乐",
+            "icon" : "http://static9.pplive.cn/pub/flagment/v_20150309153320/modules/g-1408-hd/images/logo.png",
+            "desc" : "PPTV聚力-始终和你同一频道,汇聚最清晰,最流畅的网络各类最新热门直播、点播视频。"
+        },
+
+        "request" :{
+            "cellphone_url" : "http://api.passport.pptv.com/v3/query/loginname_exist.do?logintype=username&username={}",
+            "email_url" : "http://api.passport.pptv.com/v3/query/loginname_exist.do?logintype=username&username={}",
+            "user_url" : "http://api.passport.pptv.com/v3/query/loginname_exist.do?logintype=username&username={}",
+            "method" : "GET",
+            "post_fields":{
+
+            }
+        },
+
+        "status":{
+            "judge_yes_keyword" : "<errorCode>5</errorCode>",
+            "judge_no_keyword" : "<errorCode>0</errorCode>",
+            "profile_url" : ""
+        }
+    }
+
+  - information: 插件编写者及网站所需信息；
+  - request: 核心接口定义，其中cellphone, email, user分别位手机注册、邮箱注册、用户名注册查询接口，如果仅有手机注册查询接口，其他则均为相同API即可；
+  - status: 返回结果判断，judge_yes_keyword为用户已经注册此网站，相反为未注册此网站，profile_url为预留字段；
+  
+如果接口为POST方法，则修改method为POST后，定义post_fields为参数字段，例renren：
+
+    "request" :{
+        "cellphone_url" : "http://reg.renren.com/AjaxRegisterAuth.do",
+        "email_url" : "http://reg.renren.com/AjaxRegisterAuth.do",
+        "user_url" : "http://reg.renren.com/AjaxRegisterAuth.do",
+        "method" : "POST",
+        "post_fields":{
+            "authType":"email",
+            "value":"",
+            "stage":"3"
+        }
+    },
+
+
+
+### Category
+
+ - IT
+ - 名站
+ - 娱乐
+ - 工具
+ - 教育
+ - 旅行
+ - 游戏
+ - 生活
+ - 社交
+ - 社区
+ - 购物
+ - 资讯
+ - 其它
+ - 求职招聘
---
+++ reg_test.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#author: ff0000team
+
+
+import sys
+import glob
+import json
+import requests
+import argparse
+import multiprocessing
+
+from common.color import inBlue, inRed
+from common.color import inWhite, inGreen, inYellow
+from common.output import output_init, output_finished, output_add
+
+
+def check(plugin, passport, passport_type):
+    '''
+    plugin: *.json
+    passport: username, email, phone
+    passport_type: passport type
+    '''
+    app_name = plugin['information']['name']
+    category = plugin["information"]["category"]
+    website = plugin["information"]["website"]
+    judge_yes_keyword = plugin['status']['judge_yes_keyword']
+    judge_no_keyword = plugin['status']['judge_no_keyword']
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
+
+    if plugin["request"]["{}_url".format(passport_type)]:
+        url = plugin["request"]["{}_url".format(passport_type)].format(passport)
+    else:
+        return
+
+    if plugin['request']['method'] == "GET":
+        try:
+            content = requests.get(url, headers=headers, timeout=8).content
+            content = unicode(content, "utf-8")
+        except Exception, e:
+            print e, app_name
+            return
+        if judge_yes_keyword in content and judge_no_keyword not in content:
+            print u"[{}] {}".format(category, ('%s (%s)' % (app_name, website)))
+            icon = plugin['information']['icon']
+            desc = plugin['information']['desc']
+            output_add(category, app_name, website, passport, passport_type, icon, desc)
+        else:
+            pass
+    elif plugin['request']['method'] == "POST":
+        post_data = plugin['request']['post_fields']
+        if post_data.values().count("") != 1:
+            print "The POST field can only leave a null value."
+            return
+        for k, v in post_data.iteritems():
+            if v == "":
+                post_data[k] = passport
+        try:
+            content = requests.post(url, data=post_data, timeout=8).content
+            content = unicode(content, "utf-8")
+        except Exception, e:
+            print e, app_name
+            return
+        if judge_yes_keyword in content and judge_no_keyword not in content:
+            print u"[{}] {}".format(category, ('%s (%s)' % (app_name, website)))
+            icon = plugin['information']['icon']
+            desc = plugin['information']['desc']
+            output_add(category, app_name, website, passport, passport_type, icon, desc)
+        else:
+            pass
+    else:
+        print u"{}:::Error!".format(plugin['request']['name'])
+
+
+def main():
+    reload(sys)
+    sys.setdefaultencoding("utf-8")
+    parser = argparse.ArgumentParser(description="Check how many Platforms the User registered.")
+    parser.add_argument("-u", action="store", dest="user")
+    parser.add_argument("-e", action="store", dest="email")
+    parser.add_argument("-c", action="store", dest="cellphone")
+    parser_argument = parser.parse_args()
+    banner = '''
+     .d8888b.
+    d88P  Y88b
+    Y88b.
+     "Y888b.  888d888 .d88b.  .d88b.
+        "Y88b.888P"  d8P  Y8bd88P"88b
+          "888888    88888888888  888
+    Y88b  d88P888    Y8b.    Y88b 888
+     "Y8888P" 888     "Y8888  "Y88888
+                                  888
+                             Y8b d88P
+                              "Y88P"
+    '''
+    all_argument = [parser_argument.cellphone, parser_argument.user, parser_argument.email]
+    plugins = glob.glob("./plugins/*.json")
+    print inGreen(banner)
+    print '[*] App: Search Registration'
+    print '[*] Version: V1.0(20150303)'
+    print '[*] Website: buzz.beebeeto.com'
+    file_name = ""
+    if all_argument.count(None) != 2:
+        print '\nInput "-h" view the help information.'
+        sys.exit(0)
+    if parser_argument.cellphone:
+        print inRed('\n[+] Phone Checking: %s\n') % parser_argument.cellphone
+        file_name = "cellphone_" + str(parser_argument.cellphone)
+        output_init(file_name, "Phone: ", str(parser_argument.cellphone))
+    if parser_argument.user:
+        print inRed('\n[+] Username Checking: %s\n') % parser_argument.user
+        file_name = "user_" + str(parser_argument.user)
+        output_init(file_name, "UserName: ", str(parser_argument.user))
+    if parser_argument.email:
+        print inRed('\n[+] Email Checking: %s\n') % parser_argument.email
+        file_name = "email_" + str(parser_argument.email)
+        output_init(file_name, "E-mail: ", str(parser_argument.email))
+    jobs = []
+    for plugin in plugins:
+        with open(plugin) as f:
+            try:
+                content = json.load(f)
+            except Exception, e:
+                print e, plugin
+                continue
+        if parser_argument.cellphone:
+            p = multiprocessing.Process(target=check, args=(content,unicode(parser_argument.cellphone, "utf-8"), "cellphone"))
+        elif parser_argument.user:
+            p = multiprocessing.Process(target=check, args=(content,unicode(parser_argument.user, "utf-8"), "user"))
+        elif parser_argument.email:
+            p = multiprocessing.Process(target=check, args=(content,unicode(parser_argument.email, "utf-8"), "email"))
+        p.start()
+        jobs.append(p)
+    while(sum([i.is_alive() for i in jobs]) != 0):
+        pass
+    for i in jobs:
+        i.join()
+    output_finished(file_name)
+
+
+if __name__ == '__main__':
+    main()
