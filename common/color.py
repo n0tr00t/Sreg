e2ff9ab9a7b9256f85df94d8876cc773eda4d5ff
DaveX <dave.fang@outlook.com>
2015-06-22 19:34:17 +0800

    Reformat the file (PEP8)

--- common/color.py
+++ common/color.py
@@ -2,19 +2,34 @@
 # coding=utf8
 # author=e0@2015<ff0000team>
 
+
 def inBlack(s):
-    return highlight('') + "%s[30;2m%s%s[0m"%(chr(27), s, chr(27))
+    return highlight('') + "%s[30;2m%s%s[0m" % (chr(27), s, chr(27))
+
+
 def inRed(s):
-    return highlight('') + "%s[31;2m%s%s[0m"%(chr(27), s, chr(27))
+    return highlight('') + "%s[31;2m%s%s[0m" % (chr(27), s, chr(27))
+
+
 def inGreen(s):
-    return highlight('') + "%s[32;2m%s%s[0m"%(chr(27), s, chr(27))
+    return highlight('') + "%s[32;2m%s%s[0m" % (chr(27), s, chr(27))
+
+
 def inYellow(s):
-    return highlight('') + "%s[33;2m%s%s[0m"%(chr(27), s, chr(27))
+    return highlight('') + "%s[33;2m%s%s[0m" % (chr(27), s, chr(27))
+
+
 def inBlue(s):
-    return highlight('') + "%s[34;2m%s%s[0m"%(chr(27), s, chr(27))
+    return highlight('') + "%s[34;2m%s%s[0m" % (chr(27), s, chr(27))
+
+
 def inPurple(s):
-    return highlight('') + "%s[35;2m%s%s[0m"%(chr(27), s, chr(27))
+    return highlight('') + "%s[35;2m%s%s[0m" % (chr(27), s, chr(27))
+
+
 def inWhite(s):
-    return highlight('') + "%s[37;2m%s%s[0m"%(chr(27), s, chr(27))
+    return highlight('') + "%s[37;2m%s%s[0m" % (chr(27), s, chr(27))
+
+
 def highlight(s):
-    return "%s[30;2m%s%s[1m"%(chr(27), s, chr(27))
+    return "%s[30;2m%s%s[1m" % (chr(27), s, chr(27))
